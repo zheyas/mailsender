82354b520565a75945f65290576f46bf149bce5f
Евгений <zhenyayasakov@yandex.ru>
2025-06-16 18:41:25 +0500

    Commit

--- mailings/tests.py
+++ mailings/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
 from .models import Client, Message
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 
 class ClientModelTest(TestCase):
